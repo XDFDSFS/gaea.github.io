f47d40fe1448aafd132fe3ab3850a9653aa18512
gaea3 <gm@aigaea.net>
2024-10-10 17:25:54 +0800

    Optimize the position of the scrollbar

---
+++ static/bi.2f86a0a3.js
@@ -0,0 +1,1 @@
+import"./entry.f1ff77fe.js";const i=""+globalThis.__publicAssetsURL("images/missions/bi.png");export{i as _};
---
+++ static/referral.ea6e907c.js
@@ -0,0 +1,1 @@
+import{P as e,G as n}from"./dict.2dd6cb65.js";const t=()=>n("/api/reward/referral-info"),a=r=>e("/api/network/ping",r);export{a as n,t as r};
