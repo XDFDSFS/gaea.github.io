29a18fd40eeadb43bca64863a2536debe82b9d12
gaea3 <gm@aigaea.net>
2024-10-21 16:30:36 +0800

    Optimized version 1.8

---
+++ static/referral.5d520e7d.js
@@ -0,0 +1,1 @@
+import{bk as n,bn as e}from"./entry.aea712e6.js";const t=()=>e("/api/reward/referral-info"),a=r=>n("/api/network/ping",r);export{a as n,t as r};
---
+++ static/router-login.f7184571.js
@@ -0,0 +1,1 @@
+import{bF as t,bb as o,bw as e}from"./entry.aea712e6.js";const i=t((r,s)=>{if(o().getToken){if(r.path==="/")return e("/dashboard")}else return e("/login")});export{i as default};
