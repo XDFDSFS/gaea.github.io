8b94bd0987a70c1f465958a5ae1725f5c9f93322
gaea3 <gm@aigaea.net>
2024-11-01 13:14:39 +0800

    Optimized version 3.4

---
+++ static/redirect.799f4598.js
@@ -0,0 +1,1 @@
+import{az as a,f as r,aj as s}from"./entry.843e1310.js";const u=a((o,e)=>{const t=r();if(e.query.token)return t.setToken(e.query.token),s(`/${e.query.path}`)});export{u as default};
---
+++ static/referral.fb474c0c.js
@@ -0,0 +1,1 @@
+import{a3 as a,a8 as e}from"./entry.843e1310.js";const o=()=>e("/api/reward/referral-info"),t=r=>a("/api/network/ping",r);export{t as n,o as r};
