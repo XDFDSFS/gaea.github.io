e16fdb66247f8a98a824e97e21605d668031cefb
gaea3 <gm@aigaea.net>
2024-11-14 20:59:41 +0800

    Optimized version 3.8

---
+++ static/bg-img.84baea39.js
@@ -0,0 +1,1 @@
+import"./entry.b8809691.js";const i=""+globalThis.__publicAssetsURL("images/public/bg_img.webm");export{i as _};
---
+++ static/router-login.4c70152f.js
@@ -0,0 +1,1 @@
+import{ag as o,af as e}from"./entry.b8809691.js";import{u as t}from"./user.70f06b73.js";const u=o((r,a)=>{if(t().getToken){if(r.path==="/")return e("/dashboard")}else return e("/login")});export{u as default};
