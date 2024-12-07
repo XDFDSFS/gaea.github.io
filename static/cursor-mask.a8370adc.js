d25553704e28f6d0cb37a9595e206bd76f4aaa5d
gaea3 <gm@aigaea.net>
2024-12-07 14:04:01 +0800

    Optimized version 5.0

---
+++ static/cursor-mask.a8370adc.js
@@ -0,0 +1,1 @@
+import"./entry.0c0b4405.js";const o=""+globalThis.__publicAssetsURL("images/godhood/cursor_mask.webp");export{o as _};
---
+++ static/router-login.88cb1199.js
@@ -0,0 +1,1 @@
+import{ak as o,aj as e}from"./entry.0c0b4405.js";import{u as t}from"./user.b1391fa1.js";const u=o((r,a)=>{if(t().getToken){if(r.path==="/")return e("/dashboard")}else return e("/login")});export{u as default};
