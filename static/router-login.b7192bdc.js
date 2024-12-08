5ca4724a93901a9c90325cf6be0e41da1ebcb9f6
gaea3 <gm@aigaea.net>
2024-12-08 13:38:21 +0800

    Optimized version 5.3

---
+++ static/router-login.b7192bdc.js
@@ -0,0 +1,1 @@
+import{ak as o,aj as e}from"./entry.6260d686.js";import{u as t}from"./user.e0a4c3b9.js";const u=o((r,a)=>{if(t().getToken){if(r.path==="/")return e("/dashboard")}else return e("/login")});export{u as default};
