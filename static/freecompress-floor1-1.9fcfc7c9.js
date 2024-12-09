9f4e73404b19eeab265f9d07fef5b1d8a0d9b8a9
gaea3 <gm@aigaea.net>
2024-12-09 11:49:01 +0800

    Optimized version 5.8

---
+++ static/freecompress-floor1-1.9fcfc7c9.js
@@ -0,0 +1,1 @@
+import"./entry.751b5d57.js";const s=""+globalThis.__publicAssetsURL("images/godhood/freecompress_floor1_1.png");export{s as _};
---
+++ static/router-login.6bacfcbb.js
@@ -0,0 +1,1 @@
+import{ak as o,aj as e}from"./entry.751b5d57.js";import{u as t}from"./user.05d5e88f.js";const u=o((r,a)=>{if(t().getToken){if(r.path==="/")return e("/dashboard")}else return e("/login")});export{u as default};
