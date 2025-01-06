def4a6c658121c4a3feb63151c77600e276e3ab8
gaea3 <gm@aigaea.net>
2025-01-06 17:51:56 +0800

    add godhood ID 2

---
+++ static/cursor-mask.b15f335f.js
@@ -0,0 +1,1 @@
+import"./entry.b8636d5f.js";const o=""+globalThis.__publicAssetsURL("images/godhood/cursor_mask.webp");export{o as _};
---
+++ static/mask.6a5a665d.js
@@ -0,0 +1,1 @@
+import"./entry.b8636d5f.js";const a=""+globalThis.__publicAssetsURL("images/shenGeka/mask.png");export{a as _};
