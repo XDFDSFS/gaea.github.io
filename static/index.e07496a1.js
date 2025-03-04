bf9dd5bb5b84d873e288d08c950afd6464fbe9b9
gaea3 <gm@aigaea.net>
2025-03-04 14:25:34 +0800

    add Add on chain check-in

---
+++ static/index.e07496a1.js
@@ -0,0 +1,1 @@
+import{v as o,L as a}from"./el-loading.edda49b2.js";const n={install(i){i.directive("loading",o),i.config.globalProperties.$loading=a},directive:o,service:a};export{n as E};
---
+++ static/validator.7c10fef5.js
@@ -0,0 +1,1 @@
+import{aO as i}from"./entry.38b91c68.js";const n=o=>["",...i].includes(o);export{n as i};
