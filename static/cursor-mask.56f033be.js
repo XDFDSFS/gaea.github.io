b91f3216c3c696814776e0bae33cb7d582db515f
gaea3 <gm@aigaea.net>
2025-03-06 15:48:31 +0800

    fix 1.4

---
+++ static/cursor-mask.56f033be.js
@@ -0,0 +1,1 @@
+import"./entry.2548f2a2.js";const o=""+globalThis.__publicAssetsURL("images/godhood/cursor_mask.webp");export{o as _};
---
+++ static/validator.6045ba26.js
@@ -0,0 +1,1 @@
+import{aO as i}from"./entry.2548f2a2.js";const n=o=>["",...i].includes(o);export{n as i};
