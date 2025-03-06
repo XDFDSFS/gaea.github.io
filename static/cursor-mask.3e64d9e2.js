823c001a9429fa1bf49928ddd79434ab3e600986
gaea3 <gm@aigaea.net>
2025-03-06 15:56:03 +0800

    fix 1.5

---
+++ static/cursor-mask.3e64d9e2.js
@@ -0,0 +1,1 @@
+import"./entry.5ef69f17.js";const o=""+globalThis.__publicAssetsURL("images/godhood/cursor_mask.webp");export{o as _};
---
+++ static/router-login.604ba56e.js
@@ -0,0 +1,1 @@
+import{ae as a,ad as o}from"./entry.5ef69f17.js";import{u as n}from"./user.63bf69d2.js";const d=a((r,s)=>{const t=n(),e=document.querySelector(".body-container");if(e&&e.scrollTo({top:0}),t.getToken){if(r.path==="/")return o("/dashboard")}else return o("/login")});export{d as default};
