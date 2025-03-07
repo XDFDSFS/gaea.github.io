c122b0a590b5a7f1bcbc056ef086f3fc90523f28
gaea3 <gm@aigaea.net>
2025-03-07 09:54:36 +0800

    fix 1.1

---
+++ static/redirect.7bbe264a.js
@@ -0,0 +1,1 @@
+import{c as e,o as r}from"./entry.09de6290.js";const _={__name:"redirect",setup(t){return(c,o)=>(r(),e("div"))}};export{_ as default};
---
+++ static/validator.52da2a2e.js
@@ -0,0 +1,1 @@
+import{aO as i}from"./entry.09de6290.js";const n=o=>["",...i].includes(o);export{n as i};
