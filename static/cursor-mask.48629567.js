d546434466eeee94bf565bc0581f4489db75600c
gaea3 <gm@aigaea.net>
2025-04-01 09:44:22 +0800

    Change continuous rewards to deep training 1.1

---
+++ static/cursor-mask.48629567.js
@@ -0,0 +1,1 @@
+import"./entry.f2efeee4.js";const o=""+globalThis.__publicAssetsURL("images/godhood/cursor_mask.webp");export{o as _};
---
+++ static/referral.8794145b.js
@@ -0,0 +1,1 @@
+import{P as e,G as r}from"./request.9f8f1c11.js";const o=()=>r("/api/reward/referral-info"),a=n=>e("/api/network/ping",n),s=()=>r("/api/network/ip");export{s as g,a as n,o as r};
